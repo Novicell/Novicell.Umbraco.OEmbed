e42a13ee8022b3c3d153b33d5008e5382cc3d091
Lucas Bach Bisgaard <lom@novicell.dk>
2022-08-30 12:43:24 +0200

    Remove old

--- src/Novicell.Umbraco.OEmbed.Tests/HttpClientFactory.cs
+++
@@ -1,9 +0,0 @@
-using System.Net.Http;
-
-namespace Novicell.Umbraco.OEmbed.Tests
-{
-    public class HttpClientFactory : IHttpClientFactory
-    {
-        public HttpClient CreateClient(string name) => new();
-    }
-}
--- src/Novicell.Umbraco.OEmbed/Configuration/Models/OEmbedSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Novicell.Umbraco.OEmbed.Configuration.Models
-{
-    public class OEmbedSettings
-    {
-        internal const string SectionKey = nameof(Novicell) + ":" + nameof(OEmbed);
-
-        public bool Autodiscover { get; init; }
-    }
-}
